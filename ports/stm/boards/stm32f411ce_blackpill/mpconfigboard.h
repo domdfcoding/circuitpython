f25c806e5773b783f6ad91ea5450fb210d6d01af
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-01-06 17:40:42 +0000

    Set low speed crystal for stm32 blackpill

--- ports/stm/boards/stm32f411ce_blackpill/mpconfigboard.h
+++ ports/stm/boards/stm32f411ce_blackpill/mpconfigboard.h
@@ -33,8 +33,9 @@
 #define FLASH_PAGE_SIZE             (0x4000)
 
 #define HSE_VALUE ((uint32_t)25000000)
+#define LSE_VALUE ((uint32_t)32768)
 #define BOARD_NO_VBUS_SENSE (1)
-#define BOARD_HAS_LOW_SPEED_CRYSTAL (0)
+#define BOARD_HAS_LOW_SPEED_CRYSTAL (1)
 
 // On-board flash
 // #define SPI_FLASH_MOSI_PIN          (&pin_PA07)
